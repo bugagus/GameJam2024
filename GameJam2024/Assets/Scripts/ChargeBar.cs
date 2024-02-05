791a5b012ca57d049c1f37ca0dd83d16e38b1704
SergioMC2800 <158738287+SergioMC2800@users.noreply.github.com>
2024-02-06 00:00:00 +0100

    illo4
    ChargeBar

---
+++ GameJam2024/Assets/Scripts/ChargeBar.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChargeBar : MonoBehaviour
+{
+    public Slider sliderMain;
+    public TMP_Text sliderCounter;
+    public TMP_Text SliderStatus;
+    public TMP_Text sliderColor;
+    public Gradient sliderGradientColor;
+    public Gradient sliderPlainColor;
+    public Image sliderFill;
+
+    public float sliderStartingValue;
+    public float sliderEndValue;
+    public float sliderMinValue;
+
+    public bool sliderBGFill;
+
+    void Start()
+    {
+        sliderEndValue = sliderStartingValue * 10f;
+        sliderMinValue = 0;
+        sliderCounter.text = sliderMinValue.ToString();
+        sliderMain.maxValue = sliderEndValue;
+        sliderFill.color = sliderGradientColor.Evaluate(1f);
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            sliderStartingValue = Mathf.MoveTowards(sliderStartingValue, sliderEndValue, 4f * Time.deltaTime);
+            SliderStatus.text = "Sapulsao la E";
+        }
+        else
+        {
+            sliderStartingValue = Mathf.MoveTowards(sliderStartingValue,sliderMinValue, sliderStartingValue);
+            SliderStatus.text = "Se soltó la E";
+        }
+
+        sliderMain.value = sliderStartingValue;
+        sliderCounter.text = Mathf.RoundToInt(sliderStartingValue).ToString();
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            if(sliderBGFill == false)
+            {
+                sliderBGFill = true;
+            }
+            else
+            {
+                sliderBGFill = false;
+            }
+        }
+
+        if(sliderBGFill)
+        {
+            sliderFill.color = sliderPlainColor.Evaluate(sliderMain.normalizedValue);
+            sliderColor.text = "Pone no se que de plain color en el tutorial";
+        }
+    }
+}
