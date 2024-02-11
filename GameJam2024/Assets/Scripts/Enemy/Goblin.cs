57c4d16bf7f52ec11334fb8b027ba07cf4426150
SergioMC2800 <158738287+SergioMC2800@users.noreply.github.com>
2024-02-11 18:50:53 +0100

    UwU
    Quitao el disappearText

--- GameJam2024/Assets/Scripts/Enemy/Goblin.cs
+++ GameJam2024/Assets/Scripts/Enemy/Goblin.cs
@@ -82,7 +82,6 @@ public class Goblin : MonoBehaviour
 
     public void GoAway()
     {
-        GetComponentInChildren<Animator>().SetTrigger("DisappearText");
         gameManager.RemoveGoblin(this);
         goblinTimer.SetGoingAway();
         _animator.SetBool("isWalking", true);
