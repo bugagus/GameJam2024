8e958bfbe6bf1cd07d47675aeae5777e708ff830
Agustin Triguero Noriega <101557679+bugagus@users.noreply.github.com>
2024-02-11 17:32:11 +0100

    Tutorial provisional

--- GameJam2024/Assets/Scripts/WriteText.cs
+++ GameJam2024/Assets/Scripts/WriteText.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class WriteText : MonoBehaviour
 {
     public GameObject TextPanel;
     public TMP_Text toWriteText;
+    [SerializeField] string[] texts;
+    private int textNumber;
     public bool writing;
-    float timeBetweenLetters = 0.09f;
-    int timeToEraseText = 4;
+    public float timeBetweenLetters = 0.09f;
+    public int timeToEraseText = 4;
+
+    private void Start()
+    {
+        textNumber = 0;
+        DOVirtual.DelayedCall(2.0f, ()=>{StartToWrite(texts[0]);}, false);
+    }
 
     public void StartToWrite(string text)
     {
@@ -36,6 +46,9 @@ public class WriteText : MonoBehaviour
 
             TextPanel.SetActive(false);
             writing = false;
+            textNumber++;
+            if(textNumber == texts.Length) SceneManager.LoadScene("LevelSelector");
+            StartToWrite(texts[textNumber]);
         }
         yield return null;
     }
